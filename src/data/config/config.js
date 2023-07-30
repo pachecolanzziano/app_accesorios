978d286c4d0fff8eaaf005a3d5ef4c4627851667
Luis Carlos Pacheco Lanzziano <94126564+pachecolanzziano@users.noreply.github.com>
2023-07-30 12:18:00 -0500

    produccion

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,9 +1,16 @@
+const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_NAME = process.env.DB_NAME || "bd_project_sprints";
+const DB_PORT = process.env.DB_PORT || "3306";
+
+
 module.exports={
   "development": {
-    "username": "root",
-    "password": '',
-    "database": "bd_project_sprints",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
     "dialect": "mysql"
   },
   "test":{
