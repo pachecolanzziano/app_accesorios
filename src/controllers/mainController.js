2ab552fa2e3a907b576d0ac4d72c1a1c6fd5886a
Luis Carlos Pacheco Lanzziano <pachecolanzziano@gmail.com>
2024-02-06 22:13:04 -0500

    Update mainController.js

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -8,13 +8,12 @@ const main={
     //se ocupa el método render para trabajar con view engine EJS
     index: async(req,res)=>{
         //Obtenemos el listado de productos que pertenecen a la categoría Main
-    // let productsMain= await productModel.getProductByCategory('Main')
+        let productsMain= await productModel.getProductByCategory('Main')
         //Temporalmente solo enviamos un product 
-        // let productPro=productsMain[0]
+        let productPro=productsMain[0]
         //Obtenemos el listado de productos
-        // let products= await productModel.getProducts()
-        // res.render('home',{products,productPro})
-        res.render('home',{})
+        let products= await productModel.getProducts()
+        res.render('home',{products,productPro})
     },
     
     categories:(req,res)=>{
