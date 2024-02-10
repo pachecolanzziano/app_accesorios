792de9d6d5e2b9aab0933e9c1888c6cb2ce9cea4
Luis Carlos Pacheco Lanzziano <pachecolanzziano@gmail.com>
2024-02-10 10:29:17 -0500

    datos de config DB

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,8 +1,8 @@
-const DB_HOST = process.env.DB_HOST || "127.0.0.1";
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
-const DB_NAME = process.env.DB_NAME || "bd_project_sprints";
-const DB_PORT = process.env.DB_PORT || "3306";
+const DB_HOST = process.env.MYSQLHOST || "roundhouse.proxy.rlwy.net";
+const DB_USER = process.env.MYSQLUSER || "root";
+const DB_PASSWORD = process.env.MYSQLPASSWORD || "H--5Aa53212gAg5bfgAa1HAFEbE3CeDe";
+const DB_NAME = process.env.MYSQLDATABASE || "railway";
+const DB_PORT = process.env.MYSQLPORT || "33297";
 
 module.exports={
   "development": {
@@ -30,3 +30,6 @@ module.exports={
     "dialect": "mysql"
   }
 }
+
+
+
